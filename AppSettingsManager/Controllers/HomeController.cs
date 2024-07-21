fd46bcd405afcbe798136ea305e3fafededfb9bc
anshul.singhal <singhal.s01@gmail.com>
2024-07-21 21:07:35 +0530

    Reading data from config file

--- AppSettingsManager/Controllers/HomeController.cs
+++ AppSettingsManager/Controllers/HomeController.cs
@@ -7,14 +7,26 @@ namespace AppSettingsManager.Controllers;
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly IConfiguration _configuration;
+    private TwilioSettings _twilioSettings;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
     {
         _logger = logger;
+        _configuration = configuration;
+        // bind settings to a class, instead of using settings directly
+        _twilioSettings = new TwilioSettings();
+        _configuration.GetSection("Twilio").Bind(_twilioSettings);
     }
 
     public IActionResult Index()
     {
+        ViewBag.SendGridKeys = _configuration.GetValue<string>("SendGridKeys") ?? string.Empty;
+        ViewBag.TwilioKey = _configuration.GetValue<string>("Twilio:Key") ?? string.Empty;
+        ViewBag.TwillioToken = _configuration.GetValue<string>("Twilio:Token") ?? string.Empty;
+        ViewBag.PhoneNumber = _twilioSettings.PhoneNumber; // get the value from app settings using class binding
+        ViewBag.TwilioKey = _configuration.GetSection("Twilio").GetValue<string>("Key") ?? string.Empty;
+
         return View();
     }
 
---
+++ AppSettingsManager/Models/TwilioSettings.cs
@@ -0,0 +1,8 @@
+namespace AppSettingsManager.Models;
+
+public class TwilioSettings
+{
+    public string key { get; set; }
+    public string Token { get; set; }
+    public string PhoneNumber { get; set; }
+}
